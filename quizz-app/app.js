02f96466c7c1c000321ee6b942d3d0d4e476675c
Irina Munteanu Bäck <69626975+whatthefoobar@users.noreply.github.com>
2021-09-18 15:07:24 +0200

    simple quizz function done

---
+++ quizz-app/app.js
@@ -0,0 +1,25 @@
+const correctAnswers = ['B', 'B', 'B', 'B'];
+const form = document.querySelector('.quiz-form');
+const resultBox = document.querySelector(".result");
+const resultSpan =document.querySelector(".resultSpan");
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  let score = 0;
+  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+  // check the answers
+  userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswers[index]){
+      score += 25;
+    }
+    resultBox.classList.remove("d-none");
+    resultSpan.innerHTML=` ${score}% `;
+
+  });
+
+  // log the score to console
+//   console.log(score);
+
+});
