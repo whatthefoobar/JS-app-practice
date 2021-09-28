b2120a2516feb8bf5edd434d7e083adc90a89129
Irina Munteanu Bäck <69626975+whatthefoobar@users.noreply.github.com>
2021-09-28 19:10:28 +0200

    it's alive!

--- weather-app/scripts/app.js
+++ weather-app/scripts/app.js
@@ -1,10 +1,14 @@
 const cityForm = document.querySelector("form");
 const card = document.querySelector(".card");
 const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
 
 const updateUI = (data) => {
- const cityDetails = data.cityDetails;
- const weather = data.weather;
+
+//destructuring properties
+ const {cityDetails, weather} = data;
+
  details.innerHTML = `
     <h5 class="my-3">${cityDetails.EnglishName}</h5>
     <div class="my-3">${weather.WeatherText}</div>
@@ -12,6 +16,19 @@ const updateUI = (data) => {
         <span>${weather.Temperature.Metric.Value}</span>
         <span>&deg;C</span>
     </div>`;
+
+    // update the night/day & icon images
+
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute("src", iconSrc);
+
+    let timeSrc = null;
+    if(weather.isDayTime){
+        timeSrc = "img/day.svg";
+    }else {
+        timeSrc = "img/night.svg";
+    }
+    time.setAttribute("src", timeSrc);
 
     //remove d-none class if present
     if(card.classList.contains("d-none")){
